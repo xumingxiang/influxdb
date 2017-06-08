d2e0000cbf23e1a502303d363e26d8f5947b2e68
Jonathan A. Sternberg <jonathan@influxdb.com>
2017-06-08 10:42:10 -0500

    Refactor how results are returned to the client
    This refactors the internal result returning system to match more
    closely how we iterative return points between result sets, series, and
    rows within the series. It uses the updated terminology rather than
    older terminology that no longer matches how we refer to things in the
    documentation or within the query engine.
    
    The refactor moves the aggregation and chunking behavior from
    `influxql.Emitter` to the HTTP service so that behavior is isolated to
    one location rather than sprinkled around in multiple locations. This
    refactor also allows us to use different output structures. One of these
    new output structures is implemented when using `application/x-msgpack`
    as the output format.
    
    This new format is more suited to client designs than the old JSON
    format for a few reasons. First, it maintains floats and integers as
    separate types. So, unlike JSON, you do not have to guess if a returned
    point is a float or an integer when it is a whole number. That is
    returned as part of the format.
    
    Additionally, it removes the current difficult to use chunking and
    creates a new iterative format that shouldn't be as difficult to
    process. For this new format, there is no non-chunking format.

--- influxql/iterator.gen.go
+++ influxql/iterator.gen.go
@@ -3176,8 +3176,7 @@ type floatIteratorMapper struct {
 }
 
 func newFloatIteratorMapper(itrs []Iterator, driver IteratorMap, fields []IteratorMap, opt IteratorOptions) *floatIteratorMapper {
-	e := NewEmitter(itrs, opt.Ascending, 0)
-	e.OmitTime = true
+	e := NewEmitter(itrs, opt.Ascending)
 	return &floatIteratorMapper{
 		e:      e,
 		buf:    make([]interface{}, len(itrs)),
@@ -3190,7 +3189,7 @@ func newFloatIteratorMapper(itrs []Iterator, driver IteratorMap, fields []Iterat
 }
 
 func (itr *floatIteratorMapper) Next() (*FloatPoint, error) {
-	t, name, tags, err := itr.e.loadBuf()
+	t, name, tags, err := itr.e.LoadBuf()
 	if err != nil || t == ZeroTime {
 		return nil, err
 	}
@@ -3198,7 +3197,7 @@ func (itr *floatIteratorMapper) Next() (*FloatPoint, error) {
 	itr.point.Name = name
 	itr.point.Tags = tags
 
-	itr.e.readInto(t, name, tags, itr.buf)
+	itr.e.ReadInto(t, name, tags, itr.buf)
 	if itr.driver != nil {
 		if v := itr.driver.Value(tags, itr.buf); v != nil {
 			if v, ok := v.(float64); ok {
@@ -6520,8 +6519,7 @@ type integerIteratorMapper struct {
 }
 
 func newIntegerIteratorMapper(itrs []Iterator, driver IteratorMap, fields []IteratorMap, opt IteratorOptions) *integerIteratorMapper {
-	e := NewEmitter(itrs, opt.Ascending, 0)
-	e.OmitTime = true
+	e := NewEmitter(itrs, opt.Ascending)
 	return &integerIteratorMapper{
 		e:      e,
 		buf:    make([]interface{}, len(itrs)),
@@ -6534,7 +6532,7 @@ func newIntegerIteratorMapper(itrs []Iterator, driver IteratorMap, fields []Iter
 }
 
 func (itr *integerIteratorMapper) Next() (*IntegerPoint, error) {
-	t, name, tags, err := itr.e.loadBuf()
+	t, name, tags, err := itr.e.LoadBuf()
 	if err != nil || t == ZeroTime {
 		return nil, err
 	}
@@ -6542,7 +6540,7 @@ func (itr *integerIteratorMapper) Next() (*IntegerPoint, error) {
 	itr.point.Name = name
 	itr.point.Tags = tags
 
-	itr.e.readInto(t, name, tags, itr.buf)
+	itr.e.ReadInto(t, name, tags, itr.buf)
 	if itr.driver != nil {
 		if v := itr.driver.Value(tags, itr.buf); v != nil {
 			if v, ok := v.(int64); ok {
@@ -9850,8 +9848,7 @@ type unsignedIteratorMapper struct {
 }
 
 func newUnsignedIteratorMapper(itrs []Iterator, driver IteratorMap, fields []IteratorMap, opt IteratorOptions) *unsignedIteratorMapper {
-	e := NewEmitter(itrs, opt.Ascending, 0)
-	e.OmitTime = true
+	e := NewEmitter(itrs, opt.Ascending)
 	return &unsignedIteratorMapper{
 		e:      e,
 		buf:    make([]interface{}, len(itrs)),
@@ -9864,7 +9861,7 @@ func newUnsignedIteratorMapper(itrs []Iterator, driver IteratorMap, fields []Ite
 }
 
 func (itr *unsignedIteratorMapper) Next() (*UnsignedPoint, error) {
-	t, name, tags, err := itr.e.loadBuf()
+	t, name, tags, err := itr.e.LoadBuf()
 	if err != nil || t == ZeroTime {
 		return nil, err
 	}
@@ -9872,7 +9869,7 @@ func (itr *unsignedIteratorMapper) Next() (*UnsignedPoint, error) {
 	itr.point.Name = name
 	itr.point.Tags = tags
 
-	itr.e.readInto(t, name, tags, itr.buf)
+	itr.e.ReadInto(t, name, tags, itr.buf)
 	if itr.driver != nil {
 		if v := itr.driver.Value(tags, itr.buf); v != nil {
 			if v, ok := v.(uint64); ok {
@@ -13180,8 +13177,7 @@ type stringIteratorMapper struct {
 }
 
 func newStringIteratorMapper(itrs []Iterator, driver IteratorMap, fields []IteratorMap, opt IteratorOptions) *stringIteratorMapper {
-	e := NewEmitter(itrs, opt.Ascending, 0)
-	e.OmitTime = true
+	e := NewEmitter(itrs, opt.Ascending)
 	return &stringIteratorMapper{
 		e:      e,
 		buf:    make([]interface{}, len(itrs)),
@@ -13194,7 +13190,7 @@ func newStringIteratorMapper(itrs []Iterator, driver IteratorMap, fields []Itera
 }
 
 func (itr *stringIteratorMapper) Next() (*StringPoint, error) {
-	t, name, tags, err := itr.e.loadBuf()
+	t, name, tags, err := itr.e.LoadBuf()
 	if err != nil || t == ZeroTime {
 		return nil, err
 	}
@@ -13202,7 +13198,7 @@ func (itr *stringIteratorMapper) Next() (*StringPoint, error) {
 	itr.point.Name = name
 	itr.point.Tags = tags
 
-	itr.e.readInto(t, name, tags, itr.buf)
+	itr.e.ReadInto(t, name, tags, itr.buf)
 	if itr.driver != nil {
 		if v := itr.driver.Value(tags, itr.buf); v != nil {
 			if v, ok := v.(string); ok {
@@ -16510,8 +16506,7 @@ type booleanIteratorMapper struct {
 }
 
 func newBooleanIteratorMapper(itrs []Iterator, driver IteratorMap, fields []IteratorMap, opt IteratorOptions) *booleanIteratorMapper {
-	e := NewEmitter(itrs, opt.Ascending, 0)
-	e.OmitTime = true
+	e := NewEmitter(itrs, opt.Ascending)
 	return &booleanIteratorMapper{
 		e:      e,
 		buf:    make([]interface{}, len(itrs)),
@@ -16524,7 +16519,7 @@ func newBooleanIteratorMapper(itrs []Iterator, driver IteratorMap, fields []Iter
 }
 
 func (itr *booleanIteratorMapper) Next() (*BooleanPoint, error) {
-	t, name, tags, err := itr.e.loadBuf()
+	t, name, tags, err := itr.e.LoadBuf()
 	if err != nil || t == ZeroTime {
 		return nil, err
 	}
@@ -16532,7 +16527,7 @@ func (itr *booleanIteratorMapper) Next() (*BooleanPoint, error) {
 	itr.point.Name = name
 	itr.point.Tags = tags
 
-	itr.e.readInto(t, name, tags, itr.buf)
+	itr.e.ReadInto(t, name, tags, itr.buf)
 	if itr.driver != nil {
 		if v := itr.driver.Value(tags, itr.buf); v != nil {
 			if v, ok := v.(bool); ok {
